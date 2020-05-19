e7ad8d12d4da1a5cbc259688bf3e1d4ac63e90c2
veysell <vyslbay23@gmail.com>
2020-05-19 12:03:56 +0300

    gereksiz yerler silindi

--- alarm.py
+++ alarm.py
@@ -1,37 +1,19 @@
 import time
 import os
-
+import webbrowser
 
 saat = int(input("Saat giriniz: "))
 dakika = int(input("Dakika giriniz: "))
 
 while True:
     tut = time.localtime(time.time())
+
+
     if saat == tut.tm_hour and dakika == tut.tm_min:
-#         import webbrowser
-        # strURL = "https://www.youtube.com/watch?v=p4yxn8ZahPI"
-        # webbrowser.open(strURL, new=2)
-        #
-        # SendKeys.SendKeys("{SPACE}")
-        print("alarm çaldı")
+        strURL = "https://www.youtube.com/watch?v=p4yxn8ZahPI"
+        webbrowser.open(strURL, new=2)
+        time.sleep(10)
         break
     else:
         pass
 print("başarılı")
-
-# os.system(google)
-# importing webbrowser python module
-# import webbrowser
-# #Assigning URL to be opened
-# strURL = "https://www.youtube.com/watch?v=p4yxn8ZahPI"
-# #Open url in default browser
-# webbrowser.open(strURL, new=2)
-#
-
-# import pafy
-# import vlc
-#
-# url = "https://www.youtube.com/watch?v=bMt47wvK6u0"
-# video = pafy.new(url)
-# best = video.getbest()
-# playurl = best.url
